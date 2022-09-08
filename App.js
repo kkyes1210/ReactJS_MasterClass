b4a321d29f53f9e7a18101da485c45718b1fd8c9
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-09-08 13:31:32 +0900

    2.2 Adapting and Extending
    1. add props
    2. extending component

--- App.js
+++ App.js
@@ -4,14 +4,8 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
-`;
-
-const BoxTwo = styled.div`
-  background-color: tomato;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
@@ -20,13 +14,17 @@ const Text = styled.span`
   color: white;
 `;
 
+//기존 컴포넌트 확장
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box bgColor="teal" />
+      <Box bgColor="tomato" />
+      <Circle bgColor="orange" />
     </Father>
   );
 }
