5a75a25961adf419152cca6236972e5b7f9cf1e8
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-09-12 15:10:09 +0900

    4.1 Styles

--- styled.d.ts
+++ styled.d.ts
@@ -1,9 +1,8 @@
-import "styled-components";
+import { DefaultTheme } from "styled-components";
 
-declare module "styled-components" {
-  export interface DefaultTheme {
-    textColor: string;
-    bgColor: string;
-    btnColor: string;
-  }
-}
+//https://flatuicolors.com/palette/gb
+export const theme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor: "#4cd137",
+};
