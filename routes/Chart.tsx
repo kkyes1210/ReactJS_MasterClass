59b2ca586d3c9c707586ecf673841a16919897a4
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-09-13 15:56:38 +0900

    4.14 Price Chart part Three

--- routes/Chart.tsx
+++ routes/Chart.tsx
@@ -61,6 +61,20 @@ function Chart({ coinId }: ChartProps) {
               labels: { show: false },
               axisTicks: { show: false },
               axisBorder: { show: false },
+              type: "datetime",
+              categories: data?.map((price) =>
+                new Date(price.time_close * 1000).toISOString()
+              ),
+            },
+            fill: {
+              type: "gradient",
+              gradient: { gradientToColors: ["#0be881"], stops: [0, 100] },
+            },
+            colors: ["#0fbcf9"],
+            tooltip: {
+              y: {
+                formatter: (value) => `$ ${value.toFixed(3)}`,
+              },
             },
           }}
         />
