68e2c09aa91db1298bd040fed9a1f53cd089f518
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-09-13 12:26:20 +0900

    4.9 React Query part One
    npm install react query

--- routes/coins.tsx
+++ routes/coins.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { useQuery } from "react-query";
+import { fetchCoins } from "./api";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -46,7 +48,7 @@ const Loader = styled.span`
   display: block;
 `;
 
-interface CoinInterface {
+interface ICoin {
   id: string;
   name: string;
   symbol: string;
@@ -63,29 +65,20 @@ const Img = styled.img`
 `;
 
 function Coins() {
-  const [coins, setCoins] = useState<CoinInterface[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    //()(); == 함수 즉시 실행
-    (async () => {
-      const response = await fetch("https://api.coinpaprika.com/v1/coins");
-      const json = await response.json();
-      setCoins(json.slice(0, 100));
-      setLoading(false);
-    })();
-  }, []);
+  //여기에 존재하던 긴 코드들을 react query가 대체해준다.
+  //useQuery라는 hook이 fetcher함수 fetchCoins를 불러오고 그리고 fetcher함수가 isLoading(boolean타입)이라면 실행, 그리고 json을 data에 넣어준다.
+  const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
 
   return (
     <Container>
       <Header>
         <Title>코인</Title>
       </Header>
-      {loading ? (
+      {isLoading ? (
         <Loader>Loading...</Loader>
       ) : (
         <CoinsList>
-          {coins.map((coin) => (
+          {data?.slice(0, 100).map((coin) => (
             <Coin key={coin.id}>
               <Link to={`/${coin.id}`} state={coin}>
                 <Img
