fc91979e1ddfb65fd8e77f55f16aefa2e54a3bc8
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-09-15 14:32:15 +0900

    5.13 Categories

--- components/ToDo.tsx
+++ components/ToDo.tsx
@@ -1,12 +1,39 @@
-import { IToDo } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  /* const onClick = (newCategory: IToDo["category"]) => {
+    console.log("i wanna to ", newCategory);
+  }; */
+
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    //console.log("i wanna to ", event.currentTarget.name);
+    const {
+      currentTarget: { name },
+    } = event;
+  };
   return (
     <li>
       <span>{text}</span>
-      <button>To Do</button>
-      <button>Doing</button>
-      <button>Done</button>
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+        //<button onClick={() => onClick("DOING")}>Doing</button>
+      )}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+        //<button onClick={() => onClick("TO_DO")}>To Do</button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          DONE
+        </button>
+        //<button onClick={() => onClick("DONE")}>DONE</button>
+      )}
     </li>
   );
 }
