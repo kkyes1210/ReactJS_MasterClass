b724985bc61909739fd6007c47e51df08668f628
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-09-15 14:14:38 +0900

    5.12 Refactoring

--- components/ToDoList.tsx
+++ components/ToDoList.tsx
@@ -1,60 +1,20 @@
-import { useForm } from "react-hook-form";
-import {
-  atom,
-  useRecoilState,
-  useRecoilValue,
-  useSetRecoilState,
-} from "recoil";
-
-const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
-});
-
-interface IForm {
-  toDo: string;
-}
-
-interface IToDo {
-  text: string;
-  id: number;
-  category: "DONE" | "DOING" | "TO_DO";
-}
+import { useRecoilState, useRecoilValue } from "recoil";
+import { toDoState } from "../atoms";
+import CreateToDo from "./CreateToDo";
+import ToDo from "./ToDo";
 
 function ToDoList() {
-  const [toDos, setToDos] = useRecoilState(toDoState);
-  //const [value,modFn] = useRecoilState(toDoState);
-  //const value = useRecoilValue(toDoState);
-  //const modFn = useSetRecoilState(toDoState);
-
-  const { register, handleSubmit, setValue } = useForm<IForm>();
-  const handleValid = ({ toDo }: IForm) => {
-    setToDos((oldToDos) => [
-      { text: toDo, id: Date.now(), category: "TO_DO" },
-      ...oldToDos,
-    ]);
-    //setToDos((oldToDos) => [...oldToDos]); //oldToDos의 요소들이 들어있는 배열을 반환
-    //setToDos(oldToDos =>[oldToDos]) //배열안에 배열을 담아 리턴 ==>X
-    setValue("toDo", "");
-  };
-  console.log(toDos);
+  const toDos = useRecoilValue(toDoState);
 
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
-      <form onSubmit={handleSubmit(handleValid)}>
-        <input
-          {...register("toDo", {
-            required: "Plase write a to do",
-          })}
-          placeholder="Write a to do"
-        />
-        <button>Add</button>
-      </form>
+      <CreateToDo />
       <ul>
         {toDos.map((toDo) => (
-          <li key={toDo.id}>{toDo.text}</li>
+          <ToDo {...toDo} key={toDo.id} />
+          //== <ToDo text={text} category={toDo.category} id={toDo.id}>
         ))}
       </ul>
     </div>
