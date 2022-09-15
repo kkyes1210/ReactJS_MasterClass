3af51192ed15bebb44e8b0adc71754bc43aa0791
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-09-15 15:01:39 +0900

    5.14 Immutability part One

--- components/ToDo.tsx
+++ components/ToDo.tsx
@@ -2,17 +2,25 @@ import { useSetRecoilState } from "recoil";
 import { IToDo, toDoState } from "../atoms";
 
 function ToDo({ text, category, id }: IToDo) {
-  /* const onClick = (newCategory: IToDo["category"]) => {
-    console.log("i wanna to ", newCategory);
-  }; */
-
   const setToDos = useSetRecoilState(toDoState);
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    //console.log("i wanna to ", event.currentTarget.name);
     const {
       currentTarget: { name },
     } = event;
+
+    //값 즉시 변경 or 현재 값을 argument로 주는 function 만들수 있음
+    //1. target의 경로 찾기
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      //console.log(targetIndex);
+      const oldToDo = oldToDos[targetIndex];
+      const newToDo = { text, id, category: name };
+      console.log(oldToDo, newToDo);
+      // ==> 우리가 해야할 것은 targetIndex에 있는 to do를 newToDo로 바꿔주는 것: 다음 강의
+      return oldToDos;
+    });
   };
+
   return (
     <li>
       <span>{text}</span>
@@ -20,19 +28,16 @@ function ToDo({ text, category, id }: IToDo) {
         <button name="DOING" onClick={onClick}>
           Doing
         </button>
-        //<button onClick={() => onClick("DOING")}>Doing</button>
       )}
       {category !== "TO_DO" && (
         <button name="TO_DO" onClick={onClick}>
           To Do
         </button>
-        //<button onClick={() => onClick("TO_DO")}>To Do</button>
       )}
       {category !== "DONE" && (
         <button name="DONE" onClick={onClick}>
           DONE
         </button>
-        //<button onClick={() => onClick("DONE")}>DONE</button>
       )}
     </li>
   );
