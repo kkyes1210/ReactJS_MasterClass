7cfe534bd7d34a9373dc6881878867f286772d9c
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-09-15 10:32:19 +0900

    5.7 Form Validation

--- ToDoList.tsx
+++ ToDoList.tsx
@@ -2,17 +2,37 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 function ToDoList() {
-  const { register, watch } = useForm();
-  //console.log(register("toDo"));
-  //watch는 form의 입력값들의 변화를 관찰 할 수 있게 해주는 함수
-  console.log(watch());
+  const { register, handleSubmit, formState } = useForm();
+  //handleSubmit이 validation을 담당하고 우리가 작성한 코드가 진행될 수 있게 해줄 것
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  console.log(formState.errors); //에러가 있다는 사실 + 어떤 종류의 에러인지
+
+  //handleSubmit은 두개의 인자를 받는데 하나는 데이터가 유효할 때 호출되는 함수(필수)
+  //또 다른 하나는 데이터가 유효하지 않을 때 호출되는 함수(필수X)
   return (
     <div>
-      <form>
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
         <input {...register("toDo")} placeholder="Write a to do" />
-        <input {...register("email")} placeholder="email" />
-        <input {...register("firstName")} placeholder="first Name" />
-        <input {...register("lastName")} placeholder="last Name" />
+        <input {...register("email", { required: true })} placeholder="email" />
+        <input
+          {...register("firstName", { required: true, minLength: 10 })}
+          placeholder="first Name"
+        />
+        <input
+          {...register("lastName", {
+            required: "lastName is required",
+            minLength: {
+              value: 5,
+              message: "Your lastName is too short.",
+            },
+          })}
+          placeholder="last Name"
+        />
         <button>Add</button>
       </form>
     </div>
