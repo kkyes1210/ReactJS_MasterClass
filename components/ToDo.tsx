21e61cf7e880c809e42e41f4d7149b7309512741
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-09-15 15:43:05 +0900

    5.15 Immutability part Two

--- components/ToDo.tsx
+++ components/ToDo.tsx
@@ -8,16 +8,14 @@ function ToDo({ text, category, id }: IToDo) {
       currentTarget: { name },
     } = event;
 
-    //값 즉시 변경 or 현재 값을 argument로 주는 function 만들수 있음
-    //1. target의 경로 찾기
     setToDos((oldToDos) => {
       const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
-      //console.log(targetIndex);
-      const oldToDo = oldToDos[targetIndex];
-      const newToDo = { text, id, category: name };
-      console.log(oldToDo, newToDo);
-      // ==> 우리가 해야할 것은 targetIndex에 있는 to do를 newToDo로 바꿔주는 것: 다음 강의
-      return oldToDos;
+      const newToDo = { text, id, category: name as any };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
     });
   };
 
