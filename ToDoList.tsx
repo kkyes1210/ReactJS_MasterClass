0910e93a74fbd9a635eed53607213c01c8486d48
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-09-15 12:12:47 +0900

    5.9 Custom Validation

--- ToDoList.tsx
+++ ToDoList.tsx
@@ -7,13 +7,22 @@ function ToDoList() {
     register,
     handleSubmit,
     formState: { errors },
+    setError,
   } = useForm<IForm>({
     defaultValues: {
       email: "@naver.com",
     },
   });
-  const onValid = (data: any) => {
-    console.log(data);
+
+  const onValid = (data: IForm) => {
+    if (data.password !== data.password1) {
+      setError(
+        "password1",
+        { message: "Password are not the same" },
+        { shouldFocus: true }
+      );
+    }
+    //setError("extraError", { message: "Server offline." });
   };
   console.log(errors);
 
@@ -27,6 +36,7 @@ function ToDoList() {
     lastName: string;
     password: string;
     password1: string;
+    extraError?: string; //필수항목 아님
   }
 
   return (
@@ -46,8 +56,17 @@ function ToDoList() {
           placeholder="email"
         />
         <Span>{errors?.email?.message}</Span>
+
         <input
-          {...register("firstName", { required: "Write here", minLength: 10 })}
+          {...register("firstName", {
+            required: "Write here",
+            validate: {
+              nokang: (value) =>
+                value.includes("kang") ? "no kang allowed" : true,
+              nohyun: (value) =>
+                value.includes("hyun") ? "no hyun allowed" : true,
+            },
+          })}
           placeholder="first Name"
         />
         <Span>{errors?.firstName?.message}</Span>
@@ -80,8 +99,10 @@ function ToDoList() {
         <Span>{errors?.password1?.message}</Span>
         <button>Add</button>
       </form>
+      <Span>{errors?.extraError?.message}</Span>
     </div>
   );
 }
+//errors 뒤에 물음표 붙이는 이유: 물음표를 붙이고 그 항목이 undefined이면 그 뒤를 실행하지 않음. == errors가 undefined이면 extraError를 찾지 않는다
 
 export default ToDoList;
