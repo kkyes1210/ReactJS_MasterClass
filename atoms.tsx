db614c4bf1bcbdedbdc40ec169ee77799e886e33
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-09-16 16:19:17 +0900

    5.17 Selectors part Two

--- atoms.tsx
+++ atoms.tsx
@@ -6,23 +6,22 @@ export interface IToDo {
   category: "DONE" | "DOING" | "TO_DO";
 }
 
+//사용자가 현재 선택한 카테고리 저장
+export const categoryState = atom({
+  key: "category",
+  default: "TO_DO",
+});
+
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
 });
 
-//selector를 이용하면 state를 가져다가 다른 state를 만들 수 있다.
-//==> selector는 atom의 output을 변형시키는 도구
 export const toDoSelector = selector({
   key: "toDoSelector",
   get: ({ get }) => {
     const toDos = get(toDoState);
-    //filter function은 배열에서 조건에 맞지 않는 원소들을 제거한 배열을 return
-    return [
-      toDos.filter((toDo) => toDo.category === "TO_DO"),
-      toDos.filter((toDo) => toDo.category === "DOING"),
-      toDos.filter((toDo) => toDo.category === "DONE"),
-    ];
+    const category = get(categoryState);
+    return toDos.filter((toDo) => toDo.category === category);
   },
-  //get function : 인자로 객체를 받음 {get}
 });
