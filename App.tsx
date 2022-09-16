e7255ae5560c7e2e48df101a2b91676e3eb843b4
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-09-17 00:18:05 +0900

    6.3 Drag and Drop part Two
    문제해결: index.tsx 에 있는 StrictMode 제거 했더니 제대로 실행됨.

--- App.tsx
+++ App.tsx
@@ -8,13 +8,33 @@ function App() {
     <DragDropContext onDragEnd={onDragEnd}>
       <div>
         <Droppable droppableId="one">
-          {() => (
-            <ul>
+          {(magic) => (
+            <ul ref={magic.innerRef} {...magic.droppableProps}>
               <Draggable draggableId="first" index={0}>
-                {() => <li>One</li>}
+                {(magic) => (
+                  <li
+                    ref={magic.innerRef}
+                    {...magic.draggableProps}
+                    //{...magic.dragHandleProps}
+                    //{...magic.dragHandleProps}에서 li를 움직이는 handle은 어디에서든지 가능(O,n,e 어디를 잡던지)
+                    //원한다면 또 다른 handle을 만들어서 One을 움직이기 위해 li를 전체에 클릭 허용이 아니라 움직일 수 있는 특정한 부분을 만들 수 있다.
+                  >
+                    <span {...magic.dragHandleProps}>🔥</span>
+                    One
+                  </li>
+                )}
               </Draggable>
               <Draggable draggableId="second" index={1}>
-                {() => <li>Two</li>}
+                {(magic) => (
+                  <li
+                    ref={magic.innerRef}
+                    {...magic.draggableProps}
+                    //{...magic.dragHandleProps}
+                  >
+                    <span {...magic.dragHandleProps}>🔥</span>
+                    Two
+                  </li>
+                )}
               </Draggable>
             </ul>
           )}
