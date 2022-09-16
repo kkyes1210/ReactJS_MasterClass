23efe9b73079e432a7d27eb677aec6d218e11072
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-09-16 22:28:20 +0900

    6.0 Get Selectors

--- atoms.tsx
+++ atoms.tsx
@@ -1,34 +1,14 @@
 import { atom, selector } from "recoil";
 
-//name 오류 해결 (기존에는 숫자 할당)
-export enum Categories {
-  "TO_DO" = "TO_DO",
-  "DOING" = "DOING",
-  "DONE" = "DONE",
-}
-
-export interface IToDo {
-  text: string;
-  id: number;
-  category: Categories;
-}
-
-//사용자가 현재 선택한 카테고리 저장
-export const categoryState = atom<Categories>({
-  key: "category",
-  default: Categories.TO_DO,
+export const minuteState = atom({
+  key: "minutes",
+  default: 0,
 });
 
-export const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
-});
-
-export const toDoSelector = selector({
-  key: "toDoSelector",
+export const hourSelector = selector({
+  key: "hours",
   get: ({ get }) => {
-    const toDos = get(toDoState);
-    const category = get(categoryState);
-    return toDos.filter((toDo) => toDo.category === category);
+    const minutes = get(minuteState);
+    return minutes / 60;
   },
 });
