57ce6c811d3484b35e41c72d5067f7b702db2631
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-09-16 12:25:18 +0900

    5.16 Selectors part One

--- atoms.tsx
+++ atoms.tsx
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export interface IToDo {
   text: string;
@@ -9,4 +9,20 @@ export interface IToDo {
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
+});
+
+//selector를 이용하면 state를 가져다가 다른 state를 만들 수 있다.
+//==> selector는 atom의 output을 변형시키는 도구
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    //filter function은 배열에서 조건에 맞지 않는 원소들을 제거한 배열을 return
+    return [
+      toDos.filter((toDo) => toDo.category === "TO_DO"),
+      toDos.filter((toDo) => toDo.category === "DOING"),
+      toDos.filter((toDo) => toDo.category === "DONE"),
+    ];
+  },
+  //get function : 인자로 객체를 받음 {get}
 });
--- components/ToDoList.tsx
+++ components/ToDoList.tsx
@@ -1,20 +1,34 @@
 import { useRecoilValue } from "recoil";
-import { toDoState } from "../atoms";
+import { toDoSelector, toDoState } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
-  console.log(toDos);
+  const [toDo, doing, done] = useRecoilValue(toDoSelector);
+  //console.log(selectorOutput);
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
       <CreateToDo />
+      <h2>To Do</h2>
       <ul>
-        {toDos.map((toDo) => (
+        {toDo.map((toDo) => (
           <ToDo {...toDo} key={toDo.id} />
-          //== <ToDo text={text} category={toDo.category} id={toDo.id}>
+        ))}
+      </ul>
+      <hr />
+      <h2>Doing</h2>
+      <ul>
+        {doing.map((toDo) => (
+          <ToDo {...toDo} key={toDo.id} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Done</h2>
+      <ul>
+        {done.map((toDo) => (
+          <ToDo {...toDo} key={toDo.id} />
         ))}
       </ul>
     </div>
