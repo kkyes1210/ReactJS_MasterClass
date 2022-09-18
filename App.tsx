c5dc37db533d9dac35e61e3f4d4ddfd4b88d91e8
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-09-18 13:00:05 +0900

    6.10 Cross Board Movement

--- App.tsx
+++ App.tsx
@@ -22,12 +22,14 @@ const Boards = styled.div`
   gap: 10px;
 `;
 
+//지금 다 잘 작동하지만, 빈div에 옮길 때 맨 위까지 가야지만 이동이 가능한 문제를 다음시간에 해결해 볼 것
 function App() {
   const [toDos, setToDos] = useRecoilState(toDoState);
   const onDragEnd = (info: DropResult) => {
     console.log(info);
     const { destination, draggableId, source } = info;
 
+    if (!destination) return;
     if (destination?.droppableId === source.droppableId) {
       // same board movement.
       setToDos((allBoards) => {
@@ -37,6 +39,20 @@ function App() {
         return {
           ...allBoards,
           [source.droppableId]: boardCopy,
+        };
+      });
+    }
+    if (destination?.droppableId !== source.droppableId) {
+      //cross board movement.
+      setToDos((allBoards) => {
+        const sourceBoard = [...allBoards[source.droppableId]]; //이동이 시작된 지점의 boardId
+        const destinationBoard = [...allBoards[destination?.droppableId]]; //움직임이 끝나는 boardId
+        sourceBoard.splice(source.index, 1);
+        destinationBoard.splice(destination?.index, 0, draggableId);
+        return {
+          ...allBoards,
+          [source.droppableId]: sourceBoard,
+          [destination?.droppableId]: destinationBoard,
         };
       });
     }
