8ed7fddebe0840ce370dc0c091078b394089ed53
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-09-19 18:25:09 +0900

    7.8 MotionValue part Two

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-import { motion, useMotionValue } from "framer-motion";
-import { useEffect, useRef } from "react";
+import { motion, useMotionValue, useTransform } from "framer-motion";
+import { useEffect } from "react";
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
@@ -18,17 +18,16 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-//Box를 움직여도, 컴포넌트는 재랜더링 되고 있지 않음 => useEffect 사용
 function App() {
   const x = useMotionValue(0);
+  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
   useEffect(() => {
-    x.onChange(() => console.log(x.get()));
+    scale.onChange(() => console.log(scale.get()));
   }, [x]);
 
   return (
     <Wrapper>
-      <button onClick={() => x.set(200)}>click me</button>
-      <Box drag="x" dragSnapToOrigin style={{ x }} />
+      <Box drag="x" dragSnapToOrigin style={{ x, scale }} />
     </Wrapper>
   );
 }
