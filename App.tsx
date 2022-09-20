4208073760f0f6e83bf0ae89160451e28c104006
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-09-20 13:40:20 +0900

    7.12 Slider part One

--- App.tsx
+++ App.tsx
@@ -9,6 +9,7 @@ const Wrapper = styled(motion.div)`
   justify-content: center;
   align-items: center;
   background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
+  flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
@@ -19,42 +20,57 @@ const Box = styled(motion.div)`
   position: absolute;
   top: 100px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
+  position: absolute;
 `;
 
-const boxVariants = {
-  initial: {
+const box = {
+  invisible: {
+    x: 500,
     opacity: 0,
     scale: 0,
   },
   visible: {
+    x: 0,
     opacity: 1,
     scale: 1,
-    rotateZ: 360,
+    transition: {
+      duration: 1,
+    },
   },
-  leaving: {
+  exit: {
+    x: -500,
     opacity: 0,
     scale: 0,
-    y: 30,
+    transition: {
+      duration: 1,
+    },
   },
 };
-
-//AnimatePresence : component인데 React js에서 사라지는 component를 animate
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing((prev) => !prev);
+  const [visible, setVisible] = useState(1);
+  const nextPlease = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
   return (
     <Wrapper>
-      <button onClick={toggleShowing}>Click</button>
       <AnimatePresence>
-        {showing ? (
-          <Box
-            variants={boxVariants}
-            initial="initial"
-            animate="visible"
-            exit="leaving"
-          />
-        ) : null}
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
+          i === visible ? (
+            <Box
+              variants={box}
+              initial="invisible"
+              animate="visible"
+              exit="exit"
+              key={i}
+            >
+              {i}
+            </Box>
+          ) : null
+        )}
       </AnimatePresence>
+      <button onClick={nextPlease}>next</button>
     </Wrapper>
   );
 }
