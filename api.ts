c34a8835a2892a5ad39b4fe5ecb6709b39ed8847
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-09-23 09:56:01 +0900

    8.6 Home Screen part Two
    components file move

--- api.ts
+++ api.ts
@@ -1,6 +1,25 @@
 const API_KEY = "202ccb1b66882322f27e456bb9c006c3";
 const BASE_PATH = "https://api.themoviedb.org/3";
 
+interface IMoive {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoivesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMoive[];
+  total_pages: number;
+  total_results: number;
+}
+
 export function getMovies() {
   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
     (response) => response.json()
